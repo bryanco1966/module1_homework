73f7c303b2b899a2c585f2cf2afdf539f85a39e6
Bryancombs66 <bryancombs@msn.com>
2018-03-13 22:11:32 -0600

    integer homework

---
+++ string_integer.rb
@@ -0,0 +1,33 @@
+f = "first"
+l = "last"
+
+puts "#{f}#{l}"
+puts "#{l}#{f}"
+puts "#{f}" " #{l}"
+puts "#{l} " "#{f} " "#{l} " "#{f}"
+
+name_1 = "Megan Smith"
+name_2 = "Todd Park"
+
+
+puts "#{name_1[0..5]}"
+ name_1 = name_1.split{" "}
+ puts name_1[0]
+
+ a = 12
+ b = 65
+ c = 31
+ d = 98
+
+ average = (a + b + c +d)/ 4
+ puts average
+
+ a = "Ezra".length
+ b = "Ada".length
+ c = "Yukihiro".length
+ d = "Grace".length
+
+ average_length = (a + b+ c + d)/4
+ total_length = a + b + c + d
+ puts average_length
+ puts total_length
